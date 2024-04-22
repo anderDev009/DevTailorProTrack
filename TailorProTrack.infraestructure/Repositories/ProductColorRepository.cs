95b491ed89e7ed15cf4989df4a421ddd17d7b6f7
Ander <richardanderdev009@gmail.com>
2024-04-22 13:21:52 -0400

    Creacion de repositorios de product size y product color

---
+++ TailorProTrack.infraestructure/Repositories/ProductColorRepository.cs
@@ -0,0 +1,18 @@
+﻿
+
+using TailorProTrack.domain.Entities;
+using TailorProTrack.infraestructure.Context;
+using TailorProTrack.infraestructure.Core;
+using TailorProTrack.infraestructure.Interfaces;
+
+namespace TailorProTrack.infraestructure.Repositories
+{
+    public class ProductColorRepository : BaseRepository<ProductColor>, IProductColorRepository
+    {
+        private readonly TailorProTrackContext _ctx;
+        public ProductColorRepository(TailorProTrackContext ctx) : base(ctx)
+        {
+            _ctx = ctx;
+        }
+    }
+}
---
+++ TailorProTrack.infraestructure/Repositories/ProductSizeRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using TailorProTrack.domain.Entities;
+using TailorProTrack.infraestructure.Context;
+using TailorProTrack.infraestructure.Core;
+using TailorProTrack.infraestructure.Interfaces;
+
+namespace TailorProTrack.infraestructure.Repositories
+{
+    public class ProductSizeRepository : BaseRepository<ProductSize>,IProductSizeRepository
+    {
+        private readonly TailorProTrackContext _ctx;
+        public ProductSizeRepository(TailorProTrackContext ctx) : base(ctx)
+        {
+            _ctx = ctx;
+        }
+    }
+}
