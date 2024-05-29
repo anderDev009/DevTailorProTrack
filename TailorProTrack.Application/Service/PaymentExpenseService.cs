9828d97d57a2976fa0a8b35b94aac3cbe661a3ca
Ander <richardanderdev009@gmail.com>
2024-05-28 23:28:57 -0400

    Creacion de servicio pago de gastros, modificacion de controladores y creacion del controlador

---
+++ TailorProTrack.Application/Service/PaymentExpenseService.cs
@@ -0,0 +1,26 @@
+﻿
+
+using AutoMapper;
+using TailorProTrack.Application.Contracts;
+using TailorProTrack.Application.Core;
+using TailorProTrack.Application.Dtos.Expenses.PaymentExpense;
+using TailorProTrack.Application.Service.BaseServices;
+using TailorProTrack.domain.Entities;
+using TailorProTrack.infraestructure.Interfaces;
+
+namespace TailorProTrack.Application.Service
+{
+    public class PaymentExpenseService : GenericService<PaymentExpenseDtoAdd, PaymentExpenseDtoUpdate, PaymentExpenseDtoGet, PaymentExpenses>, IPaymentExpensesService
+    {
+        private readonly IMapper _mapper;
+        private readonly IPaymentExpensesRepository _paymentExpensesRepository;
+
+        public PaymentExpenseService(IPaymentExpensesRepository repository, IMapper mapper):base(mapper,repository)
+        {
+            _mapper = mapper;
+            _paymentExpensesRepository = repository;
+        }
+      
+    }
+
+}
